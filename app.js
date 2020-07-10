69162a1d4aa90bc0170e8db1b08acc20e16ed9e3
kasamoke <40295413+kasamoke@users.noreply.github.com>
2020-07-10 20:34:39 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,27 +2,25 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql'); 
 //const getConnection = require('./dbconnection');
-const pool = require('./dbconnection');
-var val=1;
+const getConnection = require('./dbconnection');
 
-if(val === 0){
+
+// this method can be used anywhere where multiple/single queries are involved
     getConnection(function(err,connection){
+        // 1st query
         connection.query("select * from user_dat",function(err,data){
             if(err){console.log("some query error is there");}
             console.log(data);
-        connection.release();
         });
-    });
-}
-else if(val !== 0){
-    getConnection(function(err,connection){
+        // 2nd query
         connection.query("select * from reg_order_dat",function(err,data){
             if(err){console.log("some query error is there");}
             console.log(data);
             connection.release();
         });
     });
-}
+
+
 
 
 app.listen(3000, () => {
